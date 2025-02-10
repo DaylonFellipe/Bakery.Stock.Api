d48c9779292eb0e94e2f320f89d7ec22ee54c6f8
daylon <daylonfellipe3001@outlook.com>
2025-02-10 13:47:47 +0000

    RECREATED REPOSITORY

---
+++ Bakery.Stock/Daylon.Bakery.Stock.Api/Program.cs
@@ -0,0 +1,34 @@
+using Daylon.Bakery.Stock.Application;
+using Daylon.Bakery.Stock.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Application
+builder.Services.AddApplication(builder.Configuration);
+// Add Infrastructure
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//==|======>
+app.Run();
